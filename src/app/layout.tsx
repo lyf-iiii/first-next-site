a4c77739b31e8c8bdeecd071f860e55972a1d19b
lyf-iiii <1135375289@qq.com>
2026-04-09 16:32:23 +0800

    feat: test vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import MainLayout from './components/layout/MainLayout'
 import '@/styles/custom.css'
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <AntdRegistry>
           <MainLayout>{children}</MainLayout>
         </AntdRegistry>
+        <SpeedInsights />
       </body>
     </html>
   )
